d97148f9bac8555f5bad5e7efa6f2900c62435f0
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-20 14:13:46 -0400

    Update fetchers with delete and update functions

--- src/utils/fetchers.js
+++ src/utils/fetchers.js
@@ -1,7 +1,31 @@
-const url = "http://localhost:6001/listings"
+const url = "http://localhost:6001/listings/"
 
 const getListing = () => {
   return fetch(url).then((resp) => resp.json())
 }
 
-export { getListing }
+const updateListing = (id) => {
+  return fetch(url + id, {
+    method: "Patch",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(),
+  })
+    .then((resp) => resp.json())
+    .then(console.log)
+}
+
+const deleteListing = (id) => {
+  return fetch(url + id, {
+    method: "Delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .catch((error) => console.error("Error Deleting:", error))
+}
+
+export { getListing, updateListing, deleteListing }
