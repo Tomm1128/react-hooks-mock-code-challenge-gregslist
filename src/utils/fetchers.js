8a44ab38a3fd737f47148e987a2a46bfd32745e5
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-21 09:17:38 -0400

    Remove unused fetch function

--- src/utils/fetchers.js
+++ src/utils/fetchers.js
@@ -15,19 +15,6 @@ const createListing = (newListing) => {
   }).then((resp) => resp.json())
 }
 
-const updateListing = (id) => {
-  return fetch(url + id, {
-    method: "PATCH",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(),
-  })
-    .then((resp) => resp.json())
-    .then(console.log)
-}
-
 const deleteListing = (id) => {
   return fetch(url + id, {
     method: "DELETE",
@@ -39,4 +26,4 @@ const deleteListing = (id) => {
     .catch((error) => console.error("Error Deleting:", error))
 }
 
-export { getListing, createListing, updateListing, deleteListing }
+export { getListing, createListing, deleteListing }
